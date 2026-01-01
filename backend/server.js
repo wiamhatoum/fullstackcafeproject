91d2ca7e80b65e702517e9b757ed2f6ceaa85277
weam <weam@gmail.com>
2026-01-01 19:17:18 +0200

    Fix backend DB password and update frontend to use Render backend URLs

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,8 @@ app.get('/', (req, res) => {
   return  res.json("Backend is running");
 });
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
+    connectionLimit : 10,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
