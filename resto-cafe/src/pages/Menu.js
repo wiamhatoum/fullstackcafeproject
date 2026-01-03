30531bf5f2fd684771ecb10acdc7a7bada6c274e
weam <weam@gmail.com>
2026-01-03 19:09:03 +0200

    fixing menu nav

--- resto-cafe/src/pages/Menu.js
+++ resto-cafe/src/pages/Menu.js
@@ -1,15 +1,18 @@
-import React ,{ useEffect, useState } from 'react'
-import  '../App.css'
-import '../styles/menu.css'
+import React, { useEffect, useState } from "react";
+import "../App.css";
+import "../styles/menu.css";
 import MenuItem from "../components/MenuItem.js";
-import axios from 'axios';
+import axios from "axios";
 
 function Menu() {
   const [menu_items, setMenuItems] = useState([]);
+
   useEffect(() => {
     const fetchAllMenuItems = async () => {
       try {
-        const res = await axios.get("https://resto-backend-qfub.onrender.com/menu_items");
+        const res = await axios.get(
+          "https://resto-backend-qfub.onrender.com/menu_items"
+        );
         setMenuItems(res.data);
       } catch (err) {
         console.log(err);
@@ -18,35 +21,54 @@ function Menu() {
     fetchAllMenuItems();
   }, []);
 
-  const categories = [...new Set(menu_items.map(item => item.category_name))];
+  const categories = [...new Set(menu_items.map((item) => item.category_name))];
+
+  // Helper to make safe IDs
+  const getCategoryId = (category) => category.replace(/\s+/g, "-");
 
   return (
     <div className="menu-page">
+      {/* Navigation */}
       <div className="menu-section-nav">
-        {[...new Set(menu_items.map(item => item.category_name))].map((category) => (
-          <a key={category} href={`#${category}`}>
+        {categories.map((category) => (
+          <a
+            key={category}
+            href={`#${getCategoryId(category)}`}
+            onClick={(e) => {
+              e.preventDefault(); // Prevent default anchor behavior
+              const el = document.getElementById(getCategoryId(category));
+              if (el) el.scrollIntoView({ behavior: "smooth" });
+            }}
+          >
             {category}
           </a>
         ))}
       </div>
-      {categories.map((category) => (
-      <div key={category} id={category} className="menu-category">
-          <h2 className="menu-category-title">{category}</h2>
-      <div className="menu-container">
-        {menu_items.filter(item => item.category_name === category).map(item => (
-          <MenuItem
-            key={item.id}
-            image={item.image}
-            name={item.name}
-            ingredients={item.ingredients}
-            price={item.price}
-          />
-        ))}
 
-      </div>
+      {/* Menu Categories */}
+      {categories.map((category) => (
+        <div
+          key={category}
+          id={getCategoryId(category)}
+          className="menu-category"
+        >
+          <h2 className="menu-category-title">{category}</h2>
+          <div className="menu-container">
+            {menu_items
+              .filter((item) => item.category_name === category)
+              .map((item) => (
+                <MenuItem
+                  key={item.id}
+                  image={item.image}
+                  name={item.name}
+                  ingredients={item.ingredients}
+                  price={item.price}
+                />
+              ))}
+          </div>
+        </div>
+      ))}
     </div>
-    ))}
-     </div>
   );
 }
 
